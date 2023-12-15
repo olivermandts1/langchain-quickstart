fffca234de546029afe9e072514d378385e0dec1
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2023-12-15 18:07:16 +0000

    removing langchain dependency

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,31 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from openai import OpenAI
+
 st.set_page_config(page_title="🦜🔗 Quickstart App")
 st.title('🦜🔗 Quickstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-def generate_response(input_text):
-    # Specify the model name here. For example, "gpt-3.5-turbo", "text-davinci-003", etc.
-    model_name = "text-davinci-003"  # Replace with your desired model
+def generate_response(input_text, model="gpt-3.5-turbo"):
+    client = OpenAI(api_key=openai_api_key)
 
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key, model=model_name)
-    st.info(llm(input_text))
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are an AI assistant."},
+            {"role": "user", "content": input_text}
+        ]
+    )
+
+    return response.choices[0].message.content.strip('"')
 
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    text = st.text_area('Enter text:', 'Write me 1 short headline marketing SUV deals')
     submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
+
+    if submitted:
+        if not openai_api_key:
+            st.warning('Please enter your OpenAI API key!', icon='⚠️')
+        else:
+            headline = generate_response(text)
+            st.info("Generated Headline: " + headline)
