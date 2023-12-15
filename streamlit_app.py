02d7117d6aebb35f58180305235c5c6a3456115e
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2023-12-15 22:03:58 +0000

    first attempt at 1 prompt chain

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,49 @@
 import streamlit as st
 from openai import OpenAI
 
+# Set up the Streamlit page
 st.set_page_config(page_title="🦜🔗 Prompt Chaining Sandbox")
 st.title('🔗 Prompt Chaining Sandbox')
-st.caption('Create prompt chains that take outputs from 1 prompt to be used as inputs for another prompt')
+st.caption('Create prompt chains that take outputs from one prompt to be used as inputs for another prompt')
 
+# User inputs their OpenAI API key in the sidebar
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-def generate_response(input_text, model="gpt-3.5-turbo"):
+# Function to generate response using OpenAI API
+def generate_response(system_prompt, user_prompt, model="gpt-3.5-turbo", temperature=0.7):
     client = OpenAI(api_key=openai_api_key)
 
     response = client.chat.completions.create(
         model=model,
         messages=[
-            {"role": "system", "content": "You are an AI assistant."},
-            {"role": "user", "content": input_text}
-        ]
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+        temperature=temperature
     )
 
     return response.choices[0].message.content.strip('"')
 
+# Streamlit form for user input
 with st.form('my_form'):
-    option = st.selectbox(
-    'OpenAI Model',
-    ('gpt-3.5-turbo', 'gpt-4'))
+    # Model selection
+    model = st.selectbox('OpenAI Model', ('gpt-3.5-turbo', 'gpt-4'))
 
-    temperature = st.number_input('Temperature')
+    # Temperature setting
+    temperature = st.number_input('Temperature', min_value=0.0, max_value=1.0, value=0.7)
+
+    # Text inputs for system, user, and assistant prompts
+    system_prompt = st.text_area('System Prompt:', value="You are an AI assistant.")
+    user_prompt = st.text_area('User Prompt')
     
-    systemPrompt = st.text_area('System Prompt:')
-    userPrompt = st.text_area('User Prompt')
-    assistantPrompt = st.text_area('Assistant Prompt')
+    # Form submission button
     submitted = st.form_submit_button('Submit')
 
     if submitted:
         if not openai_api_key:
             st.warning('Please enter your OpenAI API key!', icon='⚠️')
         else:
-            headline = generate_response(text1)
-            st.info("Generated Headline: " + headline)
+            # Generating response
+            response = generate_response(system_prompt, user_prompt, model, temperature)
+            st.info("Generated Response: " + response)
+
