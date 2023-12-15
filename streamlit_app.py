9729808a50d1f229de227838383b2e10c7e0ec03
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2023-12-15 18:12:00 +0000

    testing form fields

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,13 @@ def generate_response(input_text, model="gpt-3.5-turbo"):
     return response.choices[0].message.content.strip('"')
 
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'Write me 1 short headline marketing SUV deals')
+    text1 = st.text_area('Enter text:', 'Write me 1 short headline marketing SUV deals')
+    text2 = st.text_area('Does this even work?')
     submitted = st.form_submit_button('Submit')
 
     if submitted:
         if not openai_api_key:
             st.warning('Please enter your OpenAI API key!', icon='⚠️')
         else:
-            headline = generate_response(text)
+            headline = generate_response(text1)
             st.info("Generated Headline: " + headline)
